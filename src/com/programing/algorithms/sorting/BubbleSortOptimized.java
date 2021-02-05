cbdf498e1066b517d732ca536b64587c41fd786a
Toshko Todorov <tttodorov13@gmail.com>
2021-02-05 15:23:17 +0200

    Add algorythm BubbleSortOptimized

---
+++ src/com/programing/algorithms/sorting/BubbleSortOptimized.java
@@ -0,0 +1,49 @@
+package com.programing.algorithms.sorting;
+
+import java.util.List;
+
+import static com.programing.algorithms.Utils.initializeIntegerList;
+import static com.programing.algorithms.Utils.printIntegerListToUi;
+
+/**
+ * For each pair of indices, swap the
+ * elements if they are out of order
+ * <p>
+ * Time Complexity
+ * Best: O(n^2)	Worst: O(n^2)	Avg: O(n^2)
+ * <p>
+ * Space Complexity: O(1)
+ * Stable: Yes
+ */
+public class BubbleSortOptimized {
+
+    // Driver method
+    public static void main(String[] args) {
+        List<Integer> list = initializeIntegerList(args);
+
+        printIntegerListToUi("Printing unsorted elements: ", list);
+
+        BubbleSortOptimized.sort(list);
+
+        printIntegerListToUi("Printing sorted elements: ", list);
+    }
+
+    // Function to sort list of integers using bubble sort
+    public static List<Integer> sort(List<Integer> list) {
+        int i, j, temp;
+        for (i = 0; i < list.size(); i++) {
+            boolean hasASwap = false;
+            for (j = 0; j < list.size() - 1 - i; j++) {
+                if (list.get(i) < list.get(j)) {
+                    temp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
+                    hasASwap = true;
+                }
+            }
+            if (!hasASwap)
+                break;
+        }
+        return list;
+    }
+}
